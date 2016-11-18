5187224d1600c96b03694ccf3eddcd8f0f88b923
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-11-18 10:12:07 +0800

    Initial Commit

---
+++ Loading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Loading
+//
+//  Created by sycf_ios on 2016/11/18.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Loading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Loading
+//
+//  Created by sycf_ios on 2016/11/18.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
